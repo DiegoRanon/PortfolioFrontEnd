2d7e55a8f15b2fcb2c41925ea87149dbf8cf88d0
Konan <mastercelis8891@gmail.com>
2023-10-16 12:05:28 -0400

    premier commit

---
+++ src/Pages/pageAccueil.js
@@ -0,0 +1,14 @@
+import React, { useState, useContext } from 'react';
+
+function pageAccueil(props) {
+
+
+    return (
+        <div className="container">
+            <h2>Page d'accueil</h2>
+        </div>
+    )
+
+}
+
+export default pageAccueil
